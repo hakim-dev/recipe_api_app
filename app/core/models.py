b45e421dd1ff1030d68f2b54658211a4e7c4c228
Hakim <hksaifee@gmail.com>
2024-01-29 20:11:50 +0530

    user model and Email normalisiation Added

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,36 @@
-from django.db import models # noqa
+"""
+Database models
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """ Manager for user """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """ Create, save and return a new user """
+        if not email:
+            raise ValueError('Users must have an email address')
+        user=self.model(email=self.normalize_email(email), **extra_fields)
+        # set the password
+        user.set_password(password)
+        # save the user
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """ User in the system """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True) # is the user active
+    is_staff = models.BooleanField(default=False) # is the user staff
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email' # email is the username
