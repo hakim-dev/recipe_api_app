c8900371851b7730a2558289e1b857a336e8cf5f
Hakim <hksaifee@gmail.com>
2024-01-29 20:52:26 +0530

    linting issue fixed

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
     """ Manager for user """
 
@@ -15,7 +16,7 @@ class UserManager(BaseUserManager):
         """ Create, save and return a new user """
         if not email:
             raise ValueError('Users must have an email address')
-        user=self.model(email=self.normalize_email(email), **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         # set the password
         user.set_password(password)
         # save the user
@@ -25,9 +26,9 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         """ Create, save and return a new superuser """
-        user=self.create_user(email, password)
-        user.is_staff=True
-        user.is_superuser=True
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
@@ -37,9 +38,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     """ User in the system """
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True) # is the user active
-    is_staff = models.BooleanField(default=False) # is the user staff
+    is_active = models.BooleanField(default=True)  # is the user active
+    is_staff = models.BooleanField(default=False)  # is the user staff
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email' # email is the username
+    USERNAME_FIELD = 'email'  # email is the username
