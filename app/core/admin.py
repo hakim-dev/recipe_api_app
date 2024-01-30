5c90122f12d0a26c66b5e475563dd9051a1c5222
Hakim <hksaifee@gmail.com>
2024-01-30 14:38:48 +0530

    Django Admin and Access Added

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,48 @@
-from django.contrib import admin # noqa
+"""
+Django admin modifications
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """
+    define admin model for user
+    """
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Personal info'),
+            {
+                'fields': ('name', )
+            }
+        ),
+        (
+            _('Permissions'),
+            {
+                'fields': ('is_active', 'is_staff', 'is_superuser')
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login', )})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': ('email',
+                       'password1',
+                       'password2',
+                       'name',
+                       'is_active',
+                       'is_staff',
+                       'is_superuser')
+        }),
+    )
+    readonly_fields = ('last_login', )
+
+admin.site.register(models.User, UserAdmin)
