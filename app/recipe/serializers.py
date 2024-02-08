bf4898a818933c1b1010dfc9b0c5f0b43add2c36
Hakim <hksaifee@gmail.com>
2024-02-08 20:32:03 +0530

    Image API Added

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,7 @@
 """
 Serializers for recipe api
 """
+from math import e
 from rest_framework import serializers
 
 from core.models import (
@@ -99,3 +100,13 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ('description',)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_fields = ('id',)
+        extra_kwargs = {'image': {'required': True}}
